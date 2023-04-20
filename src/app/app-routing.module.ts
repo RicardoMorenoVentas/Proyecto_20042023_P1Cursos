b9f4dc8c959458671c2e1aa85932937ee6bb3141
Ricardo Moreno-Ventas <ricardo.morenoventas@softtek.com>
2023-04-20 11:50:18 +0200

    Finalizado cursos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioCursoComponent } from './formulario-curso/formulario-curso.component';
+import { ListaCursosComponent } from './lista-cursos/lista-cursos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'mod-curso/:cursoID/:eliminar',title: 'Eliminar curso', component:FormularioCursoComponent},
+  {path:'mod-curso/:cursoID',title: 'Modificar curso', component:FormularioCursoComponent},
+  {path:'mod-curso',title: 'Formulario curso', component:FormularioCursoComponent},
+  {path: 'lista-cursos', title: 'Lista de cursos', component:ListaCursosComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'lista-cursos' } // Manda por defecto a lista cursos de entrada
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
